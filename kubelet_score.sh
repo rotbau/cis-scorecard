933fadcfd48c5cba7f3efdde409e3dfede84d652
Bob Bauer <rotbau@gmail.com>
2019-10-14 20:46:40 -0500

    updated

--- kubelet_score.sh
+++ kubelet_score.sh
@@ -3,7 +3,5 @@ set -o nounset
 
 RESULTS_DIR="${RESULTS_DIR:-/tmp/results}"
 
-ps -ef |grep kubelet >"${RESULTS_DIR}/kubelet_config"
+cat /etc/cni/net.d/10-nsx.conf >"${RESULTS_DIR}/ncp_version"
 echo -n "${RESULTS_DIR}/kubelet_config" >"${RESULTS_DIR}/done"
-
-
